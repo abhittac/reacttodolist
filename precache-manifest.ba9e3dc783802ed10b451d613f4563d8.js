31db195e0a04100c0b3ebf38a67ef019168400cc
unknown <abhishekchauhan3003@gmail.com>
2020-10-11 22:03:29 +0530

    Updates

---
+++ precache-manifest.ba9e3dc783802ed10b451d613f4563d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4081b4e06aa1697d497a798fd33be2e",
+    "url": "/reacttodolist/index.html"
+  },
+  {
+    "revision": "117ab5f4096c2e00e4ea",
+    "url": "/reacttodolist/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "18ae7793cd3e80813a67",
+    "url": "/reacttodolist/static/css/main.30012028.chunk.css"
+  },
+  {
+    "revision": "117ab5f4096c2e00e4ea",
+    "url": "/reacttodolist/static/js/2.056a0b41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodolist/static/js/2.056a0b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ae7793cd3e80813a67",
+    "url": "/reacttodolist/static/js/main.64e17cd8.chunk.js"
+  },
+  {
+    "revision": "afb45d3d2075ce3bcfa0",
+    "url": "/reacttodolist/static/js/runtime-main.ce85a729.js"
+  }
+]);
